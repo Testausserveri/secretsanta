f7f0a79a6f8157c50271c35bd5cd7fbb198f8cd8
Mikael <46541386+ahnl@users.noreply.github.com>
2022-11-05 15:03:55 +0200

    Add WaitingView

---
+++ ios/secretsanta/WaitingView.swift
@@ -0,0 +1,63 @@
+//
+//  WaitingView.swift
+//  secretsanta
+//
+//  Created by Mikael Hannolainen on 5.11.2022.
+//
+
+import SwiftUI
+
+struct WaitingView: View {
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Thank you for your gift!")
+                    .padding(.bottom)
+                
+                Text("Time for the exciting part: waiting for your Secret Santa!")
+                    .fontWeight(.medium)
+                    .padding(.bottom)
+               
+                Text("You will receive a notification once your gift can be redeemed.")
+                
+                Spacer()
+                
+                Image("gift-tree")
+                
+                Spacer()
+                
+                Text("Time until you can redeem your gift: 5 days 12 hours")
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                
+                HStack(alignment: .center) {
+                    Button(action: { }) {
+                        Text("Order Pick Up").frame(minWidth: 0, maxWidth: .infinity)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .buttonStyle(.borderedProminent)
+                    .controlSize(.large)
+                    .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                    
+                }
+                .frame(
+                    maxWidth: .infinity,
+                    alignment: .topLeading
+                )
+                
+            }.frame(
+                maxWidth: .infinity,
+                maxHeight: .infinity,
+                alignment: .topLeading
+            )
+            .padding([.leading, .trailing], 15)
+            .navigationBarTitle(Text("City Secret Santa"))
+        }
+    }
+}
+
+struct WaitingView_Previews: PreviewProvider {
+    static var previews: some View {
+        WaitingView()
+    }
+}
