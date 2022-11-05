f121cba788152d581d4d0a38d44b22f14cc84013
Mikael <46541386+ahnl@users.noreply.github.com>
2022-11-05 04:51:27 +0200

    Initial Commit

---
+++ secretsanta/secretsantaApp.swift
@@ -0,0 +1,17 @@
+//
+//  secretsantaApp.swift
+//  secretsanta
+//
+//  Created by Mikael Hannolainen on 5.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct secretsantaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
