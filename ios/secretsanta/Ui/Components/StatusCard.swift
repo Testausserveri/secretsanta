2be7c8433cbb33fc678e894669bd313374a94ceb
nakkikioski <dark.beach0061@fastmail.com>
2022-11-05 23:13:55 +0200

    animations

--- ios/secretsanta/Ui/Components/StatusCard.swift
+++ ios/secretsanta/Ui/Components/StatusCard.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct StatusCard: View {
+    @State private var counter1 = 35
+    
     var body: some View {
         VStack(spacing: 12) {
             Text("Pick up ordered!").font(.headline)
-            Text("Estimated time of arrival: 30 min").font(.subheadline)
+            Text("Estimated time of arrival: \(self.counter1) min")
+                .font(.subheadline)
+                .onAppear {
+                    self.runCounter(counter: self.$counter1, start: counter1, end: 0, speed: 0.087)
+                }
+            
         }.padding(25).frame(minWidth: 0, maxWidth: .infinity).background(Color(.systemGray6)).cornerRadius(18).padding()
+    }
+    
+    func runCounter(counter: Binding<Int>, start: Int, end: Int, speed: Double) {
+        counter.wrappedValue = start
+        
+        Timer.scheduledTimer(withTimeInterval: speed, repeats: true) { timer in
+            counter.wrappedValue -= 1
+            if counter.wrappedValue == end {
+                timer.invalidate()
+            }
+        }
     }
 }
 
